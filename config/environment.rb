3d9ef16855987d06c3112cce086d0f539ca875eb
aiaio <knb@alexanderinteractive.com>
2009-04-07 15:11:07 -0400

    Specified rails version

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,11 @@
 require File.join(File.dirname(__FILE__), "boot")
 
+# Specifies gem version of Rails to use when vendor/rails is not present
+RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
+
+# Bootstrap the Rails environment, frameworks, and default configuration
+require File.join(File.dirname(__FILE__), 'boot')
+
 Rails::Initializer.run do |config|
 
   # This project does not use ActiveResource.
