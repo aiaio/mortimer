9dddbddbe2063cef6ac26e8a2da5fd158c69b454
skottey <sf@alexanderinteractive.com>
2009-11-04 17:51:52 -0500

    updating password generator to include symbols

--- lib/password_generator.rb
+++ lib/password_generator.rb
@@ -4,7 +4,8 @@ class PasswordGenerator
   Numbers   = (1..10).to_a
   Lower     = ("a".."z").to_a 
   Upper     = ("A".."Z").to_a
-  Chars     = Lower + Upper + Numbers 
+  Symz     	= ["!","@","#","$","%","^","&","*"]
+  Chars     = Lower + Upper + Numbers + Symz
 
   # Password satisfies app requirements:
   # At least ten characters, at least one upper-case letter, 
@@ -12,8 +13,9 @@ class PasswordGenerator
   def self.random
     upper     = (1..2).map {|n| Upper[rand(Upper.size)]}
     numbers   = (1..2).map {|n| Numbers[rand(Numbers.size)]}
+    symz 	  = (1..2).map {|n| Symz[rand(Symz.size)]}
     remaining = (1..(rand(3) + 6)).map {|n| Chars[rand(Chars.size)]}
-    password = remaining + upper + numbers
+    password = remaining + upper + numbers + symz
     (1..password.length).map {|n| password.delete_at(rand(password.length))}.join
   end
 
